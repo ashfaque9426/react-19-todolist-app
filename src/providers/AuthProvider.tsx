f9270e32cc50b9a288eda0687bc797dc811cc7f3
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2025-04-12 19:01:13 +0600

    login, registration and logout methods are defined in AuthProvider functional component of AuthContext.

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,5 +1,6 @@
 import { useState, ReactNode, useEffect, createContext } from "react";
 import Cookies from 'js-cookie';
+import { loginUrl, logoutUrl, registerUrl } from "../constants/constants";
 
 type User = {
   userId: number,
@@ -13,11 +14,18 @@ type LoginCredentials = {
 }
 
 type RegisterCredentials = {
-  userEmail: string, 
+  userEmail: string,
   userName: string,
   userPassword: string,
   userConfirmPassword: string,
   recoveryStr: string
+}
+
+type ServRes = {
+  userData: {
+    userId: number, userEmail: string, userName: string, userSecret: string
+  },
+  errMsg: string
 }
 
 interface AuthContextType {
@@ -28,8 +36,11 @@ interface AuthContextType {
   isUserAvailable: boolean,
   setIsUserAvailable: (state: boolean) => void,
   login: (loginCredentials: LoginCredentials) => Promise<void>,
-  logout: () => Promise<void>,
+  logout: (userEmail: string) => Promise<void>,
   registerUser: (registerCredentials: RegisterCredentials) => Promise<void>,
+}
+interface ErrorHandler {
+    (err: unknown): void;
 }
 
 const AuthContext = createContext<AuthContextType | null>(null);
@@ -39,20 +50,105 @@ function AuthProvider({ children }: { children: ReactNode }) {
   const [userLoading, setUserLoading] = useState(true);
   const [isUserAvailable, setIsUserAvailable] = useState(false);
 
+  const handleErr: ErrorHandler = (err) => {
+    if (err instanceof Error) {
+        console.error(err.message);
+      } else {
+        console.error("An unknown server error occurred.");
+      }
+  }
+
+  const handleServRes = (servRes: ServRes) => {
+    if (servRes.errMsg) {
+        console.error(servRes.errMsg);
+      } else if (servRes.userData) {
+        Cookies.set('uscTDLT', servRes.userData.userSecret);
+        setUser({
+          userId: servRes.userData.userId,
+          userName: servRes.userData.userName,
+          userEmail: servRes.userData.userEmail
+        });
+      }
+  }
+
   const login = async (userCredentials: LoginCredentials) => {
-    console.log(userCredentials)
+    if (!userCredentials) {
+      console.log("User Credentials not provided as function parameter object.");
+      return;
+    }
+    try {
+      const stringifiedCrd = JSON.stringify(userCredentials);
+
+      const serverRes = await fetch(`${loginUrl}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: stringifiedCrd
+      })
+
+      const { userData, errMsg } = await serverRes.json();
+
+      handleServRes({userData, errMsg});
+    } catch (err) {
+      handleErr(err);
+    }
   }
 
   const registerUser = async (userCredentials: RegisterCredentials) => {
-    console.log(userCredentials);
+    if (!userCredentials) {
+      console.log("User Credentials not provided as function parameter object.");
+      return;
+    }
+
+    try {
+      const stringifiedCrd = JSON.stringify(userCredentials);
+
+      const serverRes = await fetch(`${registerUrl}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: stringifiedCrd
+      })
+
+      const { userData, errMsg } = await serverRes.json();
+
+      handleServRes({userData, errMsg});
+    } catch (err) {
+      handleErr(err);
+    }
   }
 
-  const logout = async () => {
-    const userSecret = Cookies.get('uscTDLT');
-    if (userSecret) {
-      Cookies.remove('uscTDLT');
-      setUser(undefined);
-      setIsUserAvailable(false);
+  const logout = async (userEmail: string) => {
+    const userSecret: string | undefined = Cookies.get('uscTDLT');
+    if (!userEmail || !userSecret) {
+      console.error(!userEmail ? "User Email parameter value is required to logout an user." : "User must be logged in to get logged out.");
+      return;
+    }
+
+    try {
+      const serverRes = await fetch(`${logoutUrl}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userEmail)
+      })
+
+      const { succMsg, errMsg } = await serverRes.json();
+
+      if (errMsg) {
+        console.error(errMsg)
+      }
+      else if (succMsg) {
+        console.log(succMsg);
+        Cookies.remove('uscTDLT');
+        setUser(undefined);
+        setIsUserAvailable(false);
+      }
+    } catch (err) {
+      handleErr(err);
     }
   }
 
@@ -67,7 +163,7 @@ function AuthProvider({ children }: { children: ReactNode }) {
   }, [user]);
 
   return (
-    <AuthContext value={{user, setUser, userLoading, setUserLoading, isUserAvailable, setIsUserAvailable, login, registerUser, logout}}>{children}</AuthContext>
+    <AuthContext value={{ user, setUser, userLoading, setUserLoading, isUserAvailable, setIsUserAvailable, login, registerUser, logout }}>{children}</AuthContext>
   )
 }
 
