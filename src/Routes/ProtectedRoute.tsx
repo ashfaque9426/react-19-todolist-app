c66615d6fa1883847966f74ab13c3910be5d8f1e
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2025-04-20 19:17:41 +0600

    ProtectedRoute code modified and some bugs fixed.

--- src/Routes/ProtectedRoute.tsx
+++ src/Routes/ProtectedRoute.tsx
@@ -21,30 +21,31 @@ function ProtectedRoute() {
     }, [expired, auth]);
 
     const token = Cookies.get('uscTDLT');
-
-    let decoded;
-
     const currentTime = Math.floor(Date.now() / 1000);
 
-    if (token) {
-        decoded = decodeJwt(token);
-    }
+    useEffect(() => {
+        if (token) {
+            const decoded = decodeJwt(token);
+            if (decoded?.exp && decoded.exp < currentTime) {
+                setExpired(true);
+            }
+        }
+    }, [token, currentTime]);
 
-    if (decoded?.exp && decoded.exp < currentTime) {
-        setExpired(true);
-    }
+    if (auth && auth.userLoading) return <div className='absolute top-0 right-0 left-0 bottom-0 bg-black opacity-35 z-50 flex justify-center items-center text-4xl text-yellow-500'>Loading....</div>
 
     if (shouldRedirect) {
         return <Navigate to="/login" state={{ from: location }} />;
     }
 
-    if (!token || !auth || !auth.isUserAvailable) {
-        return <Navigate to={"/login"} state={{ from: location }} />
+    if (!token) {
+        if (location.pathname.startsWith("/login") || location.pathname.startsWith("/register")) {
+            return <Outlet />;
+        }
+        return <Navigate to="/login" state={{ from: location }} />;
     }
 
-    if (auth && auth.userLoading) return <div className='absolute top-0 right-0 left-0 bottom-0 bg-black opacity-35 z-50 flex justify-center items-center text-4xl text-yellow-500'>Loading....</div>
-
-    if (token && (location.pathname.includes("/login") || location.pathname.includes("/register"))) {
+    if (token && (location.pathname.startsWith("/login") || location.pathname.startsWith("/register"))) {
         return <Navigate to={"/"} />
     }
 
