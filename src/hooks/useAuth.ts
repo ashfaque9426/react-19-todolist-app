2b943799e49cdcd5f424dca4efa8496fe8746141
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2025-04-28 18:37:41 +0600

    ProtectedRoute.tsx is modified to remove the unnecessary useEffect hook from it. useAuth custom hook modified to throw new Error if it will be used outside of AuthProvider.

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,6 +3,10 @@ import { AuthContext } from '../providers/AuthProvider';
 
 function useAuth() {
   const auth = use(AuthContext);
+  if (!auth) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  
   return auth;
 }
 
