f949e0cc74c875a0300da5464e3f1ae3243e57b0
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2025-05-06 18:08:25 +0600

    RegistrationForm added to /registration route and input type time value checked for later use case.

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,21 +1,32 @@
 import { Link } from "react-router";
 import FormBtn from "./FormBtn";
+import { useState } from "react";
+import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
 
 function LoginForm() {
+  const [emailInputValue, setEmailInputValue] = useState("");
+  const [passwordInputValue, setPasswordInputValue] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <form className="flex flex-col gap-5 border rounded-lg p-5 backdrop-blur-md">
       <legend className="font-bold text-2xl text-center">Login</legend>
       <div className="flex flex-col justify-center gap-10">
         {/* email */}
         <div className="flex flex-col border-b-2 pb-1.5 relative">
-          <input className="peer border-0 outline-0" type="email" name="Email" id="Email" required autoComplete="true" />
-          <label className="font-semibold text-xl absolute top-1/2 -translate-y-1/2 transition-all duration-200 peer-focus:-top-3 peer-focus:text-base" htmlFor="Email">Email</label>
+          <input className="peer border-0 outline-0" type="email" name="Email" id="Email" required autoComplete="true" onChange={e => setEmailInputValue(e.target.value)} value={emailInputValue} />
+          <label className={emailInputValue ? "font-semibold absolute -top-6 text-base" : "font-semibold text-xl absolute top-1/2 -translate-y-1/2 transition-all duration-200 peer-focus:-top-3 peer-focus:text-base"} htmlFor="Email">Email</label>
         </div>
 
         {/* password */}
         <div className="flex flex-col border-b-2 pb-1.5 relative">
-          <input className="peer border-0 outline-0" type="password" name="Password" id="Password" required autoComplete="false" />
-          <label className="font-semibold text-xl absolute top-1/2 -translate-y-1/2 transition-all duration-200 peer-focus:-top-3 peer-focus:text-base" htmlFor="Password">Password</label>
+          <input className="peer border-0 outline-0" type={showPassword ? "text" : "password"} name="Password" id="Password" required autoComplete="false" onChange={e => setPasswordInputValue(e.target.value)} value={passwordInputValue} />
+          <label className={passwordInputValue ? "font-semibold absolute -top-6 text-base" : "font-semibold text-xl absolute top-1/2 -translate-y-1/2 transition-all duration-200 peer-focus:-top-3 peer-focus:text-base"} htmlFor="Password">Password</label>
+          <span className="absolute right-0 top-1/2 -translate-y-1/2 text-white text-xl" onClick={() => setShowPassword(!showPassword)}>
+            {
+              showPassword ? <IoEyeOffOutline /> : <IoEyeOutline />
+            }
+          </span>
         </div>
 
         <div className="flex justify-between items-center">
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,3 +1,5 @@
+import RegistrationForm from "../components/RegistrationForm";
+
 function Registration() {
   return (
     <div className={`h-[calc(100vh-48px)] w-full bg-no-repeat bg-cover bg-center relative 
@@ -5,11 +7,11 @@ function Registration() {
       md:bg-[url('../assets/images/background/register-bg-tablet.jpg')] 
       lg:bg-[url('../assets/images/background/register-bg-desktop.jpg')] 
       px-2 md:px-3.5 lg:px-5 flex flex-col justify-center text-white`}>
-      <h1 id='main-component' className='text-2xl text-center font-bold my-3.5'>Register here for an account.</h1>
-      <section role="region" aria-labelledby="User-Registration">
-        <h2 id="User-Registration" className="text-2xl font-bold text-center">Please Register here.</h2>
+      
+      <section className="flex flex-col justify-center gap-5 w-full md:max-w-[400px] mx-auto z-10" role="region" aria-labelledby="User-Registration">
+        <h1 id="User-Registration" className="text-[1.7rem] font-bold text-center">Please Register here.</h1>
 
-        <form action=""></form>
+        <RegistrationForm />
       </section>
     </div>
   )
