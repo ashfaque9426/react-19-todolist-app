ac24904a16c27841b554f9c81e8b32bec3b4f888
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2025-05-19 12:20:34 +0600

    for transferent input fileds stylling issues fixed for selected input values from suggested context menu for login and registration input fields with transparent background.

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -77,19 +77,19 @@ function RegistrationForm() {
           <div className="flex flex-col justify-center gap-10">
             {/* name */}
             <div className="flex flex-col border-b-2 pb-1.5 relative">
-              <input className="peer border-0 outline-0" type="text" name="Name" id="Name" required autoComplete="true" onChange={e => setNameInputValue(e.target.value)} value={nameInputValue} />
+              <input className="for-trans-input peer border-0 outline-0" type="text" name="Name" id="Name" required autoComplete="true" onChange={e => setNameInputValue(e.target.value)} value={nameInputValue} />
               <label className={nameInputValue ? "font-semibold absolute -top-6 text-base" : "font-semibold text-xl absolute top-1/2 -translate-y-1/2 transition-all duration-200 peer-focus:-top-3 peer-focus:text-base"} htmlFor="Name">Name</label>
             </div>
 
             {/* email */}
             <div className="flex flex-col border-b-2 pb-1.5 relative">
-              <input className="peer border-0 outline-0" type="email" name="Email" id="Email" required autoComplete="true" onChange={e => setEmailInputValue(e.target.value)} value={emailInputValue} />
+              <input className="for-trans-input peer border-0 outline-0" type="email" name="Email" id="Email" required autoComplete="true" onChange={e => setEmailInputValue(e.target.value)} value={emailInputValue} />
               <label className={emailInputValue ? "font-semibold absolute -top-6 text-base" : "font-semibold text-xl absolute top-1/2 -translate-y-1/2 transition-all duration-200 peer-focus:-top-3 peer-focus:text-base"} htmlFor="Email">Email</label>
             </div>
 
             {/* password */}
             <div className="flex flex-col border-b-2 pb-1.5 relative">
-              <input className="peer border-0 outline-0" type={showPassword ? "text" : "password"} name="Password" id="Password" required autoComplete="false" onChange={e => setPasswordInputValue(e.target.value)} value={passwordInputValue} />
+              <input className="for-trans-input peer border-0 outline-0" type={showPassword ? "text" : "password"} name="Password" id="Password" required autoComplete="false" onChange={e => setPasswordInputValue(e.target.value)} value={passwordInputValue} />
               <label className={passwordInputValue ? "font-semibold absolute -top-6 text-base" : "font-semibold text-xl absolute top-1/2 -translate-y-1/2 transition-all duration-200 peer-focus:-top-3 peer-focus:text-base"} htmlFor="Password">Password</label>
               <span className="absolute right-0 top-1/2 -translate-y-1/2 text-white text-xl" onClick={() => setShowPassword(!showPassword)}>
                 {
@@ -100,7 +100,7 @@ function RegistrationForm() {
 
             {/* confirm password */}
             <div className="flex flex-col border-b-2 pb-1.5 relative">
-              <input className="peer border-0 outline-0" type={showConfirmPass ? "text" : "password"} name="confirmPassword" id="confirmPassword" required autoComplete="false" onChange={e => setConfirmPassInputValue(e.target.value)} value={confirmPassInputValue} />
+              <input className="for-trans-input peer border-0 outline-0" type={showConfirmPass ? "text" : "password"} name="confirmPassword" id="confirmPassword" required autoComplete="false" onChange={e => setConfirmPassInputValue(e.target.value)} value={confirmPassInputValue} />
               <label className={confirmPassInputValue ? "font-semibold absolute -top-6 text-base" : "font-semibold text-xl absolute top-1/2 -translate-y-1/2 transition-all duration-200 peer-focus:-top-3 peer-focus:text-base"} htmlFor="confirmPassword">Confirm Password</label>
               <span className="absolute right-0 top-1/2 -translate-y-1/2 text-white text-xl" onClick={() => setShowConfirmPass(!showConfirmPass)}>
                 {
